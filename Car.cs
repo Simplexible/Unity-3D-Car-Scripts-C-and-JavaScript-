e48a20f9b1bd58914b50aca6abb1dc9b6c40a4be
Simplexible <snakenas@hotmail.co.uk>
2016-11-17 02:51:11 +0000

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+	private Rigidbody carRigid;
+	public int speed;
+	public int rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		carRigid = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate(){
+		if (Input.GetButton("Forward")){
+			carRigid.AddForce (transform.right * -speed * Time.deltaTime);
+}
+		if (Input.GetButton("Reverse")){
+			carRigid.AddForce (transform.right * speed * Time.deltaTime);
+		}
+
+		if (Input.GetButton("Left")){
+			transform.Rotate (0, -rotateSpeed, 0);
+		}
+
+		if (Input.GetButton("Right")){
+			transform.Rotate (0, rotateSpeed, 0);
+		}
+		if (Input.GetButton("Reset")){
+			Application.LoadLevel ("Menu");
+		}
+		if (Input.GetButton("Recover")){
+			Application.LoadLevel ("Scene2");
+		}
+		if (Input.GetButton("Upfly")){
+			transform.Rotate (0, 0, rotateSpeed);
+		}	
+		if (Input.GetButton("Downfly")){
+			transform.Rotate (0, 0, -rotateSpeed);
+		}
+
+	}
+}
---
+++ Carfly.js
@@ -0,0 +1,12 @@
+﻿function Update ()
+{
+		if(transform.position.x >490)
+	{
+		transform.position.x = 80.7;
+		transform.position.y = 201.43;
+		transform.position.z = 23.7;
+		transform.rotation.x = 0;
+		transform.rotation.y = 0;
+		transform.rotation.z = 0;
+	}
+	}
